23560909a0ddb7331df52ebd45b572c5f8d1959a
CayoPOliveira <cayoproliveira@gmail.com>
2025-06-22 21:39:36 -0300

    feat: add initial tests for preprocessAudio and bufferToStream functions

---
+++ specs/baileys/helpers/preprocessAudio.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it } from "bun:test";
+
+describe("preprocessAudio", () => {
+  describe("#preprocessAudio", () => {
+    describe("when the audio is in wav format", () => {
+      it.todo("returns the processed audio in wav format", () => {
+        // Test logic to be implemented
+      });
+    });
+    describe("when the audio is in mp3-low format", () => {
+      it.todo("returns the processed audio in mp3-low format", () => {
+        // Test logic to be implemented
+      });
+    });
+    describe("when the audio is in mp3-high format", () => {
+      it.todo("returns the processed audio in mp3-high format", () => {
+        // Test logic to be implemented
+      });
+    });
+    describe("when an error occurs during processing", () => {
+      it.todo("throws an error", () => {
+        // Test logic to be implemented
+      });
+    });
+  });
+  describe("#bufferToStream", () => {
+    it.todo("converts a buffer to a readable stream", () => {
+      // Test logic to be implemented
+    });
+  });
+});
