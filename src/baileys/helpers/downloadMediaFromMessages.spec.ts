379e0bff8b2d036c0f9c73d53bd8a7177de1fc96
CayoPOliveira <cayoproliveira@gmail.com>
2025-06-24 18:38:18 -0300

    refactor: simplify test cases by removing unnecessary function bodies

--- src/baileys/helpers/downloadMediaFromMessages.spec.ts
+++ src/baileys/helpers/downloadMediaFromMessages.spec.ts
@@ -2,62 +2,26 @@ import { describe, it } from "bun:test";
 
 describe("downloadMediaFromMessages", () => {
   describe("#downloadMediaFromMessages", () => {
-    it.todo("returns null if messages array is empty", () => {
-      // Test logic to be implemented
-    });
-    it.todo("skips messages without key.id or message object", () => {
-      // Test logic to be implemented
-    });
-    it.todo("downloads media and save it to a file", () => {
-      // Test logic to be implemented
-    });
-    it.todo("returns base64 data when includeMedia is true", () => {
-      // Test logic to be implemented
-    });
-    it.todo("preprocess audio messages", () => {
-      // Test logic to be implemented
-    });
-    it.todo("handles errors during media download", () => {
-      // Test logic to be implemented
-    });
-    it.todo("returns null if no media was downloaded", () => {
-      // Test logic to be implemented
-    });
+    it.todo("returns null if messages array is empty");
+    it.todo("skips messages without key.id or message object");
+    it.todo("downloads media and save it to a file");
+    it.todo("returns base64 data when includeMedia is true");
+    it.todo("preprocess audio messages");
+    it.todo("handles errors during media download");
+    it.todo("returns null if no media was downloaded");
   });
 
   describe("#extractMediaMessage", () => {
-    it.todo("extract media from an imageMessage", () => {
-      // Test logic to be implemented
-    });
-
-    it.todo("extract media from a stickerMessage", () => {
-      // Test logic to be implemented
-    });
-
-    it.todo("extract media from a videoMessage", () => {
-      // Test logic to be implemented
-    });
-
-    it.todo("extract media from an audioMessage", () => {
-      // Test logic to be implemented
-    });
-
-    it.todo("extract media from a documentMessage", () => {
-      // Test logic to be implemented
-    });
-
-    it.todo("media from a documentWithCaptionMessage", () => {
-      // Test logic to be implemented
-    });
-
-    it.todo("returns null for messages without media mapped", () => {
-      // Test logic to be implemented
-    });
+    it.todo("extract media from an imageMessage");
+    it.todo("extract media from a stickerMessage");
+    it.todo("extract media from a videoMessage");
+    it.todo("extract media from an audioMessage");
+    it.todo("extract media from a documentMessage");
+    it.todo("media from a documentWithCaptionMessage");
+    it.todo("returns null for messages without media mapped");
   });
 
   describe("#streamToBuffer", () => {
-    it.todo("converts a stream to buffer", () => {
-      // Test logic to be implemented
-    });
+    it.todo("converts a stream to buffer");
   });
 });
