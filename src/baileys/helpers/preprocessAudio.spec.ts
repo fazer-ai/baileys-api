f4414230357179f894b9a61e42d2f509ef3f160e
CayoPOliveira <cayoproliveira@gmail.com>
2025-06-24 18:41:50 -0300

    refactor: simplify test cases by removing unnecessary function bodies

--- src/baileys/helpers/preprocessAudio.spec.ts
+++ src/baileys/helpers/preprocessAudio.spec.ts
@@ -3,29 +3,23 @@ import { describe, it } from "bun:test";
 describe("preprocessAudio", () => {
   describe("#preprocessAudio", () => {
     describe("when the audio is in wav format", () => {
-      it.todo("returns the processed audio in wav format", () => {
-        // Test logic to be implemented
-      });
+      it.todo("returns the processed audio in wav format");
     });
+
     describe("when the audio is in mp3-low format", () => {
-      it.todo("returns the processed audio in mp3-low format", () => {
-        // Test logic to be implemented
-      });
+      it.todo("returns the processed audio in mp3-low format");
     });
+
     describe("when the audio is in mp3-high format", () => {
-      it.todo("returns the processed audio in mp3-high format", () => {
-        // Test logic to be implemented
-      });
+      it.todo("returns the processed audio in mp3-high format");
     });
+
     describe("when an error occurs during processing", () => {
-      it.todo("throws an error", () => {
-        // Test logic to be implemented
-      });
+      it.todo("throws an error");
     });
   });
+
   describe("#bufferToStream", () => {
-    it.todo("converts a buffer to a readable stream", () => {
-      // Test logic to be implemented
-    });
+    it.todo("converts a buffer to a readable stream");
   });
 });
