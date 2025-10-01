34fbb0fe43e0a49f17032b52e47e472762e155f2
gabrieljablonski <contact@gabrieljablonski.com>
2025-09-30 22:37:30 -0300

    fix: usage of `it.todo`

--- src/helpers/promisify.spec.ts
+++ src/helpers/promisify.spec.ts
@@ -2,6 +2,9 @@ import { describe, it } from "bun:test";
 
 describe("promisify", () => {
   describe("#promisify", () => {
-    it.todo("returns object with promisse, resolve and reject methods");
+    it.todo(
+      "returns object with promisse, resolve and reject methods",
+      () => {},
+    );
   });
 });
